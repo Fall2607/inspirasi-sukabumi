957eb7c5e0371472e890fedcb1553f571545dc5c
Fallen <habibnaufal621@gmail.com>
2024-09-05 07:36:17 +0700

    up img

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -37,7 +37,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="./pages/billing.html">
+                <a class="nav-link " href="#">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-folder-17 text-primary text-sm opacity-10"></i>
                     </div>
